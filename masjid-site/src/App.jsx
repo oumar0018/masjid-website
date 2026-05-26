e5b5864a05995a48af83f6b99214cd760497e45d
Umar <umarisa.isa@gmail.com>
2026-05-26 22:44:06 +0100

    Rename admin.jsx to Admin.jsx

--- masjid-site/src/App.jsx
+++ masjid-site/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MasjidApp from "./MasjidApp";
-import Admin from "./Admin";
+import admin from "./admin";
 import VideoGallery from './components/Videos';
 // Inside your <Routes> component, add:
 
@@ -16,7 +16,7 @@ function App() {
         <Route path="/videos" element={<VideoGallery />} />
 
         {/* Admin dashboard */}
-        <Route path="/admin" element={<Admin />} />
+        <Route path="/admin" element={<admin />} />
       </Routes>
     </Router>
   );
