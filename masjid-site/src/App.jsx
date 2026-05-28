450959c905e2e3fe413504ad4f01c2c83e41c896
Umar <umarisa.isa@gmail.com>
2026-05-29 00:55:37 +0100

    admin changes caps

--- masjid-site/src/App.jsx
+++ masjid-site/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MasjidApp from "./MasjidApp";
-import Admin from "./admin";
+import Admin from "./Admin";
 import VideoGallery from './components/Videos';
 // Inside your <Routes> component, add:
 
@@ -16,7 +16,7 @@ function App() {
         <Route path="/videos" element={<VideoGallery />} />
 
         {/* Admin dashboard */}
-        <Route path="/admin" element={<Admin />} />
+        <Route path="/Admin" element={<Admin />} />
       </Routes>
     </Router>
   );
